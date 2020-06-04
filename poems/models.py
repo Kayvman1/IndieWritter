b424655981bf54de536e513c7171aaa1a6299f9d
Kayvamn1 <kayvanfouladi@gmail.com>
2020-06-04 19:37:27 -0400

    2 hours 50 min

--- poems/models.py
+++ poems/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+import random
 
 # Create your models here.
 class Poem(models.Model):
     title = models.CharField(max_length = 120, null = False, blank  = False)
     content = models.TextField() 
     image = models.FileField(upload_to = 'images/', blank =True, null =True  )
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'content' : self.content,
+            'likes' : random.randint(0 , 200)
+        }
--- poems/views.py
+++ poems/views.py
@@ -14,20 +14,23 @@ def poem_create_view(request, *args, **kwargs):
     form = PoemForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
-        obj = form.save (commit= False)
+        obj = form.save(commit=False)
+        # do other form related logic
         obj.save()
-        if request.is_ajax() == True:
-            return JsonResponse({}, status = 201 )
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201) # 201 == created items
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = PoemForm()
-
-    return render (request, "components/form.html", context = {'form':form})
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+    return render(request, 'components/form.html', context={"form": form})
 
 
 def poem_list_view(request, *args, **kwargs):
     qs = Poem.objects.all()
-    poem_list = [{"id": obj.id, "title": obj.title,"content": obj.content, "likes": random.randint(0, 100)} for obj in qs]
+    poem_list = [obj.serialize() for obj in qs]
     data = {
         'response' : poem_list
     }
