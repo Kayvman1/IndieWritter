8becead2ba5f2e3b73ddd1efbf20591bf5bf0006
Kayvamn1 <kayvanfouladi@gmail.com>
2020-06-05 23:26:00 -0400

    doint this agina

---
+++ poems/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.conf import settings
+from .models import Poem
+
+class PoemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Poem
+        fields = ['title', 'content']
+    
+    def validate_content(self, value):
+        if 'bitch' in value:
+            raise forms.ValidationError("Explicit Langauge")
+        return value
+    
+    def validate_title(self, value):
+        if len(value) > settings.MAX_TITLE_LEN:
+            raise forms.ValidationError("Your Title is Too Long")
+        return value
